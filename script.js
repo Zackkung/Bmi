3fb20f0ebbc3f20176dcc3c22261af172194399b
Zackkung <122410400+Zackkung@users.noreply.github.com>
2023-05-30 20:22:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ function calculateBMI() {
   
   let genderSelect = document.querySelector("#gender").value;
   console.log(genderSelect)
-  if (genderSelect == "Male") {
+  if (genderSelect == "ขาย") {
     MaleBmiCalc(bmi);
   }
-  else if (genderSelect == "Female") {
+  else if (genderSelect == "หญิง") {
     FemaleBmiCalc(bmi);
   }
   var resultElement = document.getElementById('result');
-  resultElement.innerHTML = 'Your BMI: ' + bmi.toFixed(5);
+  resultElement.innerHTML = ' ค่า BMI ของคุณ: ' + bmi.toFixed(5);
 }
 
 var calculateButton = document.getElementById('btn');
@@ -21,37 +21,37 @@ calculateButton.addEventListener('click', calculateBMI);
 function MaleBmiCalc(bmi) {
   var result2 = document.getElementById("result2");
   if (bmi <= 13.30) {
-    
+    result2.innerText = "ผลลัพธ์: ผอมมาก";
   }
   else if (bmi <= 18.57) {
-    result2.innerText = "Result: Under weight";
+    result2.innerText = "ผลลัพธ์: ผอม";
   }
   else if (bmi <= 23.60) {
-    result2.innerText = "Result: Normal";
+    result2.innerText = "ผลลัพธ์: สมส่วน";
   }
   else if (bmi <= 28.20) {
-    result2.innerText = "Result: Over weight";
+    result2.innerText = "ผลลัพธ์: ท้วม";
   }
   else if (bmi >= 28.21) {
-    
+    result2.innerText = "ผลลัพธ์: อ้วน";
   }
 };
 
 function FemaleBmiCalc(bmi) {
   var result2 = document.getElementById("result2");
   if (bmi <= 13.88) {
-    
+    result2.innerText = "ผลลัพธ์: ผอมมาก";
   }
   else if (bmi <= 20.06) {
-    result2.innerText = "Result: Under weight";
+    result2.innerText = "ผลลัพธ์: ผอม";
   }
   else if (bmi <= 24.34) {
-    result2.innerText = "Result: Normal";
+    result2.innerText = "ผลลัพธ์: สมส่วน";
   }
   else if (bmi <= 28.47) {
-    result2.innerText = "Result: Over weight";
+    result2.innerText = "ผลลัพธ์: ท้วม";
   }
   else if (bmi >= 28.48) {
-    
+    result2.innerText = "ผลลัพธ์: อ้วน";
   }
 };
